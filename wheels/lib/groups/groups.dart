bb47e0131ca65d73fa69db8c54461e3203e460a2
miguelMBAir <miguelagc@outlook.es>
2021-11-07 14:35:33 -0500

    version estable

--- wheels/lib/groups/groups.dart
+++ wheels/lib/groups/groups.dart
@@ -2,7 +2,6 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:wheels/models/groupModel.dart';
 import 'package:wheels/sharedPreferences/preferences.dart';
 
 class Groups extends StatefulWidget {
